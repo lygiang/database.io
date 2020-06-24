084a34ff61f9aef4c08d2ed51b21089e958a1ab1
lygiang <lyhuonggiang28.jp@gmail.com>
2020-06-24 10:07:59 +0700

    update code mongodb

--- index.js
+++ index.js
@@ -5,7 +5,22 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + "/public"));
 
-MongoClient.connect("mongodb://localhost:27017",(err,client) => {
+//Nhúng bodyParser vào app
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//Đọc file ejs
+app.set("view engine", "ejs")
+
+app.get("/", function (req, res) {
+    res.render("index.ejs")
+})
+
+//Chạy localhost cổng 3000
+app.listen(3000, function () {
+    console.log("Hello nodejs running on port 3000")
+})
+
+MongoClient.connect("mongodb://localhost:27017", (err, client) => {
     if (err) {
         return console.log(err)
     }
@@ -13,6 +28,7 @@ MongoClient.connect("mongodb://localhost:27017",(err,client) => {
     console.log("Đã kết nối tới Database")
 })
 
+//////////////////////////////////////////////////////////////////////////
 let tourList = [{
     link: "./detail_sapa.html",
     img: "https://img.thuthuatphanmem.vn/uploads/2018/09/26/anh-dep-mot-goc-thi-xa-sa-pa-xe-chieu_052040737.jpg",
@@ -33,50 +49,76 @@ let tourList = [{
 }
 ]
 
-app.use(bodyParser.urlencoded({extended:true}))
 
-app.set("view engine", "ejs")
+//code xử lý việc lấy dữ liệu từ mongoDB đổ vào nodejs app
+app.get("/todo", function (req, res) {
+    let tourList = db.collection("website").find().toArray().then(results => {
+        console.log(results)
+        res.render("index.ejs", { result: results })
+    }).catch(error => {
+        console.error(error)
+    })
+    //console.log(__dirname)
+    //res.render("index.ejs", {result : quotes})
+})
 
-// app.get("/", function(req, res){
-//     res.render("index.ejs")
-// })
+// app.get('/todo/:todoId', function (req, res) {
+//     let id = req.params['todoId'];
+//     let objectId = require('mongodb').ObjectID
+//     db.collection("website").findOne({ _id: new objectId(id) }).then(results => {
+//         console.log(results)
+//         res.render("index.ejs", { todo: results })
+//     }).catch(error => {
+//         console.error(error)
+//     })
+// });
 
-// app.get("/about", function(req, res){
-//     res.send("<h1>This is the about function</h1>")
-// })
-
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     // console.log(__dirname)
     // res.sendFile(__dirname + "/index.html")
-    res.render("index.ejs", { result: tourList})
+    res.render("index.ejs", { result: results })
 })
 
-app.post("/new-todo", function(req, res) {
-    console.log("Đã nhận request", req.body)
-    let newTodo = req.body;
-    tourList.push(newTodo);
+app.post("/add-tour", function (req, res) {
+    db.collection("website").insertOne(req.body).then(results => {
+        console.log(results)
+    }).catch(error => {
+        console.error(error)
+    })
 })
+    // console.log("Đã nhận request", req.body)
+    // let newTodo = req.body;
+    // push(newTodo);
 
 
-app.post("/fix-todo", function(req, res) {
-    console.log("Đã nhận request", req.body)
-    for (var i = 0; i < tourList.length; i++) {
-        if (tourList[i].title === req.body.title) {
-            tourList[i].title = req.body.newtitle
-        }        
-    }
+
+app.post("/update-tour", function (req, res) {
+    db.collection("website").updateOne({ title: req.body.title }, { $set: { title: req.body.newtitle } }).then(results => {
+        console.log(results);
+    }).catch(error => {
+        console.error(error)
+    })
 })
+    // console.log("Đã nhận request", req.body)
+    // for (var i = 0; i < tourList.length; i++) {
+    //     if (tourList[i].title === req.body.title) {
+    //         tourList[i].title = req.body.newtitle
+    //     }
+    // }
 
-app.post("/delete-todo", function(req, res) {
-    console.log("Đã nhận request", req.body)
-    for (var i = 0; i < tourList.length; i++) {
-        if (tourList[i].title === req.body.title) {
-            tourList.splice(i, 1);
-        }
-    }
+
+app.post("/delete-tour", function (req, res) {
+    db.collection("website").deleteOne({ title: req.body.title }).then(results => {
+        console.log(results)
+    }).catch(error => {
+        console.error(error)
+    })
 })
+    // console.log("Đã nhận request", req.body)
+    // for (var i = 0; i < tourList.length; i++) {
+    //     if (tourList[i].title === req.body.title) {
+    //         tourList.splice(i, 1);
+    //     }
+    // }
 
 
-app.listen(3000, function() {
-    console.log("Hello nodejs running on port 3000")
-})
